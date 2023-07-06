6ba4b9a37cad1bde9285d696819b8eba078ba9f4
Edgar Rojas <edgarrojas2904@gmail.com>
2023-07-06 05:13:31 -0400

    Improve offline functionality in Verify Register and Mark as Out

--- src/context/VerificationContext.js
+++ src/context/VerificationContext.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
-import React, { createContext, useContext, useEffect, useRef, useState} from 'react';
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
 import { BASE_URL } from '../config'
 import Toast from 'react-native-toast-message'
 import { ROUTES } from '../constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthContext } from './AuthContext';
 import { useNavigation } from '@react-navigation/native';
+import NetInfo from "@react-native-community/netinfo";
+// import { useNetInfo } from '@react-native-community/netinfo';
 
 export const VerificationContext = createContext()
 
@@ -21,20 +23,93 @@ export const VerificationProvider = ({ children }) => {
     const [calibres, setCalibres] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [products, setProducts] = useState([])
+    const [isConected, setIsConected] = useState(true)
+
+    const [dataToVerifyOffline, setDataToVerifyOffline] = useState([])
+    const [dataMarkAsOutOffline, setDataMarkAsOutOffline] = useState([])
 
     const [listCanastillas, setListCanastillas] = useState([])
     const [listCanastillasSalidas, setListCanastillasSalidas] = useState([])
 
-    const [selected, setSelected] = useState([])    
+    const [selected, setSelected] = useState([])
     const navigation = useNavigation();
+
+    useEffect(() => {
+        const unsubscribe = NetInfo.addEventListener((state) => {
+            setIsConected(state.isInternetReachable);
+        });
+
+        reloadOfflineInfo()
+        
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (isConected) {
+            reloadOfflineInfo()
+        }
+        return () => {
+
+        };
+    }, [isConected]);
+
+    const reloadOfflineInfo = () => {
+        AsyncStorage.getItem('dataToVerifyOffline').then((datos) => {
+            if (datos) {
+                setDataToVerifyOffline(JSON.parse(datos))
+                startOfflineVerification(JSON.parse(datos))
+            }
+        })
+
+        AsyncStorage.getItem('dataMarkAsOutOffline').then((datos) => {
+            if (datos) {
+                setDataMarkAsOutOffline(JSON.parse(datos))
+                startOfflineMasAsOut(JSON.parse(datos))
+            }
+        })
+    }
+
+    const startOfflineVerification = async (data) => {
+        const removedCodes = []
+        if (isConected) {
+            for (let i = 0; i < data.length; i++) {
+                const code = data[i];
+                let result = await verifyCode(code, true)
+                if (result) {
+                    removedCodes.push(code)
+                }
+            }
+        }
+        const dataWithoutCodes = dataToVerifyOffline.filter(c => !removedCodes.includes(c));
+        setDataToVerifyOffline(dataWithoutCodes)
+        await AsyncStorage.setItem('dataToVerifyOffline', JSON.stringify(dataWithoutCodes))
+    }
+
+    const startOfflineMasAsOut = async (data) => {
+        const removedCodes = []
+        if (isConected) {
+            for (let i = 0; i < data.length; i++) {
+                const code = data[i];
+                let result = await markAsOut(code, true)
+                if (result) {
+                    removedCodes.push(code)
+                }
+            }
+        }
+        const dataWithoutCodes = dataMarkAsOutOffline.filter(c => !removedCodes.includes(c));
+        setDataMarkAsOutOffline(dataWithoutCodes)
+        await AsyncStorage.setItem('dataMarkAsOutOffline', JSON.stringify(dataWithoutCodes))
+    }
 
     const { token } = useContext(AuthContext)
 
     const fetchCounters = async () => {
-        try {            
+        try {
             const response = await axios.get(`${BASE_URL}/verificaciones/count`)
-            const data = response.data            
-            if (data.status) {                
+            const data = response.data
+            if (data.status) {
                 setBulbos(data.data.bulbos)
                 setCanastillas(data.data.canastillas)
                 AsyncStorage.setItem('totalBulbos', `${data.data.bulbos}`)
@@ -51,7 +126,7 @@ export const VerificationProvider = ({ children }) => {
     }
 
     const fetchDataSelects = async () => {
-        try{
+        try {
             const bodegasResponse = await axios.post(`${BASE_URL}/get/bodegas`)
             setBodegas(bodegasResponse.data.data)
 
@@ -62,7 +137,7 @@ export const VerificationProvider = ({ children }) => {
             setProveedores(proveedoresResponse.data.data)
 
             const tiposResponse = await axios.post(`${BASE_URL}/get/tipos`)
-            setTipos(tiposResponse.data.data)                        
+            setTipos(tiposResponse.data.data)
 
             const calibresResponse = await axios.post(`${BASE_URL}/get/calibres`)
             setCalibres(calibresResponse.data.data)
@@ -73,34 +148,34 @@ export const VerificationProvider = ({ children }) => {
     }
 
     const fetctVariedades = async (tipo) => {
-        try{
+        try {
             setIsLoading(true)
             setVariedades([])
             const variedadesResponse = await axios.post(`${BASE_URL}/get/variedades`, { tipo })
             setVariedades(variedadesResponse.data.data)
             setIsLoading(false)
-        } catch (e){
+        } catch (e) {
             setIsLoading(false)
         }
     }
 
     const fetctColores = async (variedad) => {
-        try{
+        try {
             setIsLoading(true)
             setColores([])
             const coloresResponse = await axios.post(`${BASE_URL}/get/colores`, { variedad })
             setColores(coloresResponse.data.data)
             setIsLoading(false)
-        } catch (e){
+        } catch (e) {
             setIsLoading(false)
         }
     }
 
     const createRegister = async (payload, id = null) => {
-        try{
+        try {
             setIsLoading(true)
-            const url = `${BASE_URL}/products` + ((id) ? `/${id}`: '')            
-            const response = await axios.post(url, {...payload})
+            const url = `${BASE_URL}/products` + ((id) ? `/${id}` : '')
+            const response = await axios.post(url, { ...payload })
             const data = response.data
             if (data.status) {
                 Toast.show({
@@ -129,7 +204,7 @@ export const VerificationProvider = ({ children }) => {
     }
 
     const deleteRegister = async (id) => {
-        try{
+        try {
             setIsLoading(true)
             const url = `${BASE_URL}/products/${id}`
             const response = await axios.delete(url,)
@@ -161,18 +236,18 @@ export const VerificationProvider = ({ children }) => {
     }
 
     const fetchProducts = async () => {
-        try{
+        try {
             setIsLoading(true)
             const response = await axios.get(`${BASE_URL}/products`)
             const data = response.data
-            if (data.status){
-                setProducts(data.data)                
+            if (data.status) {
+                setProducts(data.data)
                 AsyncStorage.setItem('products', JSON.stringify(data.data))
             }
             setIsLoading(false)
-        } catch(e){
+        } catch (e) {
             data = await AsyncStorage.getItem('products')
-            if(data){
+            if (data) {
                 setProducts(data)
             }
             setIsLoading(false)
@@ -180,11 +255,11 @@ export const VerificationProvider = ({ children }) => {
     }
 
     const fetchCanastillas = async () => {
-        try{
+        try {
             setIsLoading(true)
             const response = await axios.get(`${BASE_URL}/canastillas`)
             const data = response.data
-            if (data.status){
+            if (data.status) {
                 setListCanastillas(data.data)
             }
             setIsLoading(false)
@@ -194,11 +269,11 @@ export const VerificationProvider = ({ children }) => {
     }
 
     const fetchCanastillasSalidas = async () => {
-        try{
+        try {
             setIsLoading(true)
             const response = await axios.get(`${BASE_URL}/canastillas/salidas`)
             const data = response.data
-            if (data.status){
+            if (data.status) {
                 setListCanastillasSalidas(data.data)
             }
             setIsLoading(false)
@@ -207,73 +282,157 @@ export const VerificationProvider = ({ children }) => {
         }
     }
 
-    const verifyCode = async (code) => {
+    const verifyCode = async (code, fromOffline = false) => {
         try {
-            setIsLoading(true)
-            const response = await axios.post(`${BASE_URL}/canastillas/mark/out/code`, {codigo: code})
-            const data = response.data
+            if (isConected) {
+                setIsLoading(true)
+                const response = await axios.post(`${BASE_URL}/canastillas/verify/code`, { codigo: code })
+                const data = response.data
 
-            setIsLoading(false)
-            if(data.status){
-                Toast.show({
-                    type: 'success',
-                    text1: 'Verificar Canastilla',
-                    text2: data.message,
-                    autoHide: true,
-                    visibilityTime: 2500,
-                    position: 'bottom'
-                })
-                
-                return true;
+                setIsLoading(false)
+                if (data.status) {
+                    Toast.show({
+                        type: 'success',
+                        text1: 'Verificar Canastilla',
+                        text2: data.message,
+                        autoHide: true,
+                        visibilityTime: 2500,
+                        position: 'bottom'
+                    })
+                } else {
+                    Toast.show({
+                        type: 'error',
+                        text1: 'Verificar Canastilla',
+                        text2: data.message,
+                        autoHide: true,
+                        visibilityTime: 2500,
+                        position: 'bottom'
+                    })
+                }
+
+                return true
             } else {
-                Toast.show({
-                    type: 'error',
-                    text1: 'Verificar Canastilla',
-                    text2: data.message,
-                    autoHide: true,
-                    visibilityTime: 2500,
-                    position: 'bottom'
-                })
-                return false
-            }            
-        } catch(e) {
+                if (!fromOffline) {
+                    addCodeToVerifyOffline(code)
+                    return false
+                }
+            }
+        } catch (e) {
             setIsLoading(false)
+            console.log(e)
+            Toast.show({
+                type: 'error',
+                text1: 'Verificar Canastilla',
+                text2: 'Ocurrio un error, intente de nuevo',
+                autoHide: true,
+                visibilityTime: 2500,
+                position: 'bottom'
+            })
             return false
         }
     }
 
-    const markAsOut = async (code) => {
-        try {
-            setIsLoading(true)
-            const response = await axios.post(`${BASE_URL}/canastillas/mark/out/code`, {codigo: code})
-            const data = response.data
+    const addCodeToVerifyOffline = (code) => {
+        const filtredData = dataToVerifyOffline.find(c => code == c);
+        if (filtredData == code) {
+            Toast.show({
+                type: 'error',
+                text1: 'Verificar Canastilla',
+                text2: `OFFLINE: Codigo ${code} ya se encuentra registrado para ser verificado`,
+                autoHide: false,
+                position: 'bottom'
+            })
+        } else {
+            Toast.show({
+                type: 'success',
+                text1: 'Verificar Canastilla',
+                text2: `OFFLINE: Codigo ${code} registrado para ser verificado`,
+                autoHide: false,
+                position: 'bottom'
+            })
+            const newData = [...dataToVerifyOffline, code]
+            AsyncStorage.setItem('dataToVerifyOffline', JSON.stringify(newData))
+            setDataToVerifyOffline(newData);
+        }
+    }
 
-            setIsLoading(false)
-            if(data.status){
-                Toast.show({
-                    type: 'success',
-                    text1: 'Verificar Canastilla',
-                    text2: data.message,
-                    autoHide: true,
-                    visibilityTime: 2500,
-                    position: 'bottom'
-                })
-                
-                return true;
+    const markAsOut = async (code, fromOffline = false) => {
+        try {
+            if (isConected) {
+                setIsLoading(true)
+                const response = await axios.post(`${BASE_URL}/canastillas/mark/out/code`, { codigo: code })
+                const data = response.data
+
+                setIsLoading(false)
+                if (data.status) {
+                    Toast.show({
+                        type: 'success',
+                        text1: 'Registrar Salida',
+                        text2: data.message,
+                        autoHide: true,
+                        visibilityTime: 2500,
+                        position: 'bottom',
+                        props: {
+                            text2NumberOfLines: 2 //number of how many lines you want
+                        }
+                    })
+                } else {
+                    Toast.show({
+                        type: 'error',
+                        text1: 'Registrar Salida',
+                        text2: data.message,
+                        autoHide: true,
+                        visibilityTime: 2500,
+                        position: 'bottom',
+                        props: {
+                            text2NumberOfLines: 2 //number of how many lines you want
+                        }
+                    })
+                }
+
+                return true
             } else {
-                Toast.show({
-                    type: 'error',
-                    text1: 'Verificar Canastilla',
-                    text2: data.message,
-                    autoHide: true,
-                    visibilityTime: 2500,
-                    position: 'bottom'
-                })
+                if (!fromOffline) {
+                    addCodeToMarkAsOutOffline(code)
+                }
                 return false
-            }            
-        } catch(e) {
+            }
+        } catch (e) {
+            console.log(e)
             setIsLoading(false)
+            Toast.show({
+                type: 'error',
+                text1: 'Registrar Salida',
+                text2: 'Ocurrio un error, intente de nuevo.',
+                autoHide: true,
+                visibilityTime: 2500,
+                position: 'bottom'
+            })
             return false
+        }
+    }
+
+    const addCodeToMarkAsOutOffline = (code) => {
+        const filtredData = dataMarkAsOutOffline.find(c => code == c);
+        if (filtredData == code) {
+            Toast.show({
+                type: 'error',
+                text1: 'Registrar Salida',
+                text2: `OFFLINE: Codigo ${code} ya se encuentra registrado para registrar su salida`,
+                autoHide: false,
+                position: 'bottom'
+            })
+        } else {
+            Toast.show({
+                type: 'success',
+                text1: 'Registrar Salida',
+                text2: `OFFLINE: Codigo ${code} registrado para registrar su salida`,
+                autoHide: false,
+                position: 'bottom'
+            })
+            const newData = [...dataMarkAsOutOffline, code]
+            AsyncStorage.setItem('dataMarkAsOutOffline', JSON.stringify(newData))
+            setDataMarkAsOutOffline(newData);
         }
     }
 
@@ -284,12 +443,12 @@ export const VerificationProvider = ({ children }) => {
     useEffect(() => {
         fetchDataSelects()
     }, [])
-    
+
     return (
-        <VerificationContext.Provider 
-            value={{ 
-                bulbos, 
-                canastillas, 
+        <VerificationContext.Provider
+            value={{
+                bulbos,
+                canastillas,
                 isLoading,
                 bodegas,
                 productos,
@@ -302,10 +461,12 @@ export const VerificationProvider = ({ children }) => {
                 selected,
                 listCanastillas,
                 listCanastillasSalidas,
+                dataToVerifyOffline,
+                dataMarkAsOutOffline,
                 fetchCanastillas,
                 fetchCanastillasSalidas,
                 selectProduct,
-                fetchCounters, 
+                fetchCounters,
                 fetchDataSelects,
                 fetctVariedades,
                 fetctColores,
@@ -315,7 +476,7 @@ export const VerificationProvider = ({ children }) => {
                 verifyCode,
                 markAsOut
             }}>
-            { children }
+            {children}
         </VerificationContext.Provider>
     );
 }

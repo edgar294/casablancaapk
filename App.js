333551fe1ef559f7a58ea8679b6b794a0d319e8d
Edgar Rojas <edgarrojas2904@gmail.com>
2023-07-06 05:11:50 -0400

    Improve Toast

--- App.js
+++ App.js
@@ -3,15 +3,39 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import AuthNavigator from './src/navigations/AuthNavigator';
 import { AuthProvider } from './src/context/AuthContext';
-import Toast from 'react-native-toast-message';
+import Toast, { ErrorToast, SuccessToast } from 'react-native-toast-message';
 
 export default function App() {
-
+    const toastConfig = {
+        success: (props) => (
+            <SuccessToast
+                {...props}
+                // style={styles.style}
+                // contentContainerStyle={styles.contentContainerStyle}
+                // text1Style={styles.text1Style}
+                // text1NumberOfLines={2}
+                // text2Style={styles.text2Style}
+                text2NumberOfLines={2}
+                
+            />
+        ),
+        error: (props) => (
+            <ErrorToast
+                {...props}
+                // style={[styles.style, styles.errorStyle]}
+                // contentContainerStyle={styles.contentContainerStyle}
+                // text1Style={styles.text1Style}
+                // text1NumberOfLines={2}
+                // text2Style={styles.text2Style}
+                text2NumberOfLines={2}
+            />
+        ),
+    };
     return (
         <NavigationContainer>
             <AuthProvider>
                 <AuthNavigator />
-                <Toast />
+                <Toast config={toastConfig} ref={(ref) => Toast.setRef(ref)} />
             </AuthProvider>
         </NavigationContainer>
     );

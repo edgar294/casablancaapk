795be67b8092f31f6a56fd6ce3227129eb03a477
Edgar Rojas <edgarrojas2904@gmail.com>
2023-11-04 04:41:00 -0400

    Update config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'http://192.168.140.238/api'
+// export const BASE_URL = 'http://192.168.0.107/api'
 // export const BASE_URL = 'https://casablanca.edgarr.site/api'
 export const BASE_URL = 'https://inventariosrf.com/api'
